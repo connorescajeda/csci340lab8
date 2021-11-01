f13d1f50695d3cc4590fdf6668257dd5e9e8792e
Connor <escajedacj@hendrix.edu>
2021-11-01 13:39:05 -0500

    Finished step 3

---
+++ RazorPagesLab8/RazorPagesLab8/Pages/About.cshtml.cs
@@ -0,0 +1,37 @@
+using RazorPagesLab8.Models.SchoolViewModels;
+using RazorPagesLab8.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RazorPagesLab8.Models;
+
+namespace RazorPagesLab8.Pages
+{
+    public class AboutModel : PageModel
+    {
+        private readonly SchoolContext _context;
+
+        public AboutModel(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<EnrollmentDateGroup> Students { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+
+            Students = await data.AsNoTracking().ToListAsync();
+        }
+    }
+}
